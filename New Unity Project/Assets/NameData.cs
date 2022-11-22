b012a30ee4ed9f66efc7ff113972c8e4e89ed95b
Luã Maury Maquiné da Silva <83655458+luamaquine@users.noreply.github.com>
2022-11-22 10:03:55 -0400

    Search function add

--- New Unity Project/Assets/NameData.cs
+++ New Unity Project/Assets/NameData.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
--- New Unity Project/Assets/SendData.cs
+++ New Unity Project/Assets/SendData.cs
@@ -1,30 +1,36 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using MongoDB.Driver;
-using UnityEditor.PackageManager;
 using UnityEngine.UI;
 
 public class SendData : MonoBehaviour
 {
     public TMP_InputField TextName;
     public TMP_InputField TextIdade;
+    public TMP_InputField TextPesquisar;
     public Text Textshowname;
+    public Text Textshowname2;
+    public Text TextShowIdade;
 
     private string nameToDatabase;
     private string idadeToDatabase;
     public DatabaseConnection DatabaseConnection;
     public GameObject panelHistorico;
+    public GameObject panelPesquisar;
     public List<string> documents;
-
+    public GameObject closepanel;
 
     private void Start()
     {
         DatabaseConnection.GetDatabase();
         ShowHistory();
+        ShowName();
+       
     }
+
+    
 
     public void SendInfo()
     {
@@ -59,5 +65,39 @@ public class SendData : MonoBehaviour
         panelHistorico.SetActive(true);
     }
 
-    
+    public void closePanel()
+    {
+        panelHistorico.SetActive(false);
+        panelPesquisar.SetActive(false);
+    }
+
+    public void showPanelPesquisar()
+    {
+        panelPesquisar.SetActive(true);
+    }
+
+    public void ShowName()
+    {
+        var pesquisar = TextPesquisar.text;
+        var filtro2 = Builders<NameData>.Filter.Where(x => x.Name == pesquisar);
+        IAsyncCursor<NameData> dt2 = DatabaseConnection._nameData.FindSync(filtro2);
+        string nome = "";
+        string idade = "";
+        dt2.ForEachAsync((bsonDoc) =>
+        {
+            nome = bsonDoc.Name;
+            idade = bsonDoc.Idade;
+        });
+        
+        var filtro3 = Builders<NameData>.Filter.Where(x => x.Idade == pesquisar);
+        IAsyncCursor<NameData> dt3 = DatabaseConnection._nameData.FindSync(filtro3);
+        dt3.ForEachAsync((bsonDoc) =>
+        {
+            nome = bsonDoc.Name;
+            idade = bsonDoc.Idade;
+        });
+        
+        Textshowname2.text = nome;
+        TextShowIdade.text = idade;
+    }
 }
